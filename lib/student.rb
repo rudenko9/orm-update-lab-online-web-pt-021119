d5565dad0ec80294504096ae4b350d9611dca73f
Nataliya R <n.rudenko@gmail.com>
2019-05-02 00:13:09 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,69 @@
+require_relative "../config/environment.rb"
+
+class Student
+attr_accessor :name, :grade, :id 
+
+def initialize(name, grade, id = nil)
+  @name = name
+  @grade = grade 
+  @id = id
+end 
+
+def self.create_table
+  sql = <<-SQL
+  CREATE TABLE students( id INTEGER PRIMARY KEY,
+  name TEXT, grade INTEGER)
+  SQL
+  DB[:conn].execute(sql)
+ end 
+ 
+ def self.drop_table
+   sql = <<-SQL
+   DROP TABLE students 
+   SQL
+   
+   DB[:conn].execute(sql)
+ end 
+ 
+ def update
+   sql = <<-SQL
+   UPDATE students SET name = ?, grade = ? WHERE id = ?
+   SQL
+   DB[:conn].execute(sql, self.name, self.grade, self.id)
+   
+ end 
+ 
+
+def save
+  if self.id
+  self.update
+else
+  sql = <<-SQL
+  INSERT INTO students (name, grade) VALUES (?, ?)
+  SQL
+  
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+end
+end 
+
+def self.create(name, grade)
+  new_student = Student.new(name,grade)
+  new_student.save 
+  new_student
+end
+
+def self.new_from_db(array)
+  
+  
+end
+
+def self.find_by_name(name)
+ sql = <<-SQL
+ SELECT * FROM students WHERE name = ? LIMIT 1
+ SQL
+ DB[:conn].exwcute(sql,mame).map |row|
+ self.new_from_db(row)
+end 
+  
+end
