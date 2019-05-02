b128e94cec91de66ac53acfea9d06feedbd4193d
Nataliya R <n.rudenko@gmail.com>
2019-05-02 00:16:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,8 +62,8 @@ def self.find_by_name(name)
  sql = <<-SQL
  SELECT * FROM students WHERE name = ?
  SQL
- DB[:conn].execute(sql, name).map {|row|
- self.new_from_db(row)
+ DB[:conn].execute(sql, name).map {|row| 
+ 
 end
   
 end
