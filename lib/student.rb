9ced2e4395b54db2020db5ff49b1615aa3b94fad
Nataliya R <n.rudenko@gmail.com>
2019-05-02 00:16:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ end
 
 def self.find_by_name(name)
  sql = <<-SQL
- SELECT * FROM students WHERE name = ? LIMIT 1
+ SELECT * FROM students WHERE name = ?
  SQL
  DB[:conn].execute(sql,name).map |row|
  self.new_from_db(row)
