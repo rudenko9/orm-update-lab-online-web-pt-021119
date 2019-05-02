dda06ed512fc55d449fa12651edf1f9fabe144c3
Nataliya R <n.rudenko@gmail.com>
2019-05-02 00:19:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,8 +56,8 @@ def self.find_by_name(name)
  SELECT * FROM students WHERE name = ?
  SQL
  DB[:conn].execute(sql, name).map {|row| new_from_db(row)}.first 
- 
 end
+
  def update
    sql = <<-SQL
    UPDATE students SET name = ?, grade = ? WHERE id = ?
