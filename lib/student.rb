c5f3f92d69596235491d4ba910f7e346b370ea6c
Nataliya R <n.rudenko@gmail.com>
2019-05-02 00:18:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,9 +44,11 @@ def self.create(name, grade)
   new_student
 end
 
-def self.new_from_db(array)
-  
-  
+def self.new_from_db(row)
+  id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
 end
 
 def self.find_by_name(name)
